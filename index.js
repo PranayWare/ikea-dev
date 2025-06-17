a7a808bb7702acb07dc935008d38933ec7ec4758
PranayWare <pranayware898@gmail.com>
2025-06-17 06:19:14 +0530

    Push full app files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+mongoose.connect('mongodb://mongo:27017/testdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/', (req, res) => {
+  res.send('🚀 Node.js + MongoDB running in Docker Compose!');
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
